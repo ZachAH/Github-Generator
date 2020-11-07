457b4354ce999c297b48afd56746173ce434a951
ZMAN <zachh@github.com>
2020-11-07 11:36:48 -0600

    final node changes to idex file added more css

---
+++ src/webpage.js
@@ -0,0 +1,117 @@
+
+const generateTeam = team => {
+
+   
+    const generateManager = manager => {
+        return `
+        <div class="card employee-card">
+        <div class="card-header bg-dark">
+            <h2 class="card-title text-white">${manager.getName()}</h2>
+            <h3 class="card-title text-white"><i class="fas fa-mug-hot mr-2 bg-dark"></i>${manager.getRole()}</h3>
+        </div>
+        <div class="card-body bg-dark">
+            <ul class="list-group">
+                <li class="list-group-item">ID: ${manager.getId()}</li>
+                <li class="list-group-item">Email: <a href="mailto:${manager.getEmail()}}">${manager.getEmail()}</a></li>
+                <li class="list-group-item">Office number: ${manager.getOfficeNumber()}}</li>
+            </ul>
+        </div>
+    </div>
+        `;
+    };
+
+    
+    const generateIntern = intern => {
+        return `
+        <div class="card employee-card">
+    <div class="card-header bg-dark">
+        <h2 class="card-title text-white">${intern.getName()}</h2>
+        <h3 class="card-title text-white"><i class="fas fa-user-graduate mr-2 bg-dark"></i>${intern.getRole()}</h3>
+    </div>
+    <div class="card-body bg-dark">
+        <ul class="list-group">
+            <li class="list-group-item">ID: {{ id }}</li>
+            <li class="list-group-item">Email: <a href="mailto:${intern.getEmail()}">${intern.getEmail()}</a></li>
+            <li class="list-group-item">School: ${intern.getSchool()}</li>
+        </ul>
+    </div>
+    </div>
+        `;
+    };
+    
+    const generateEngineer = engineer => {
+        return `
+        <div class="card employee-card">
+    <div class="card-header bg-dark">
+        <h2 class="card-title text-white">${engineer.getName()}</h2>
+        <h3 class="card-title text-white"><i class="fas fa-glasses mr-2 bg-dark"></i>${engineer.getRole()}</h3>
+    </div>
+    <div class="card-body bg-dark">
+        <ul class="list-group">
+            <li class="list-group-item">ID: ${engineer.getId()}</li>
+            <li class="list-group-item">Email: <a href="mailto:${engineer.getEmail()}">${engineer.getEmail()}</a></li>
+            <li class="list-group-item">GitHub: <a href="https://github.com/${engineer.getGithub()}" target="_blank" rel="noopener noreferrer">${engineer.getGithub()}</a></li>
+        </ul>
+    </div>
+</div>
+        `;
+    };
+
+
+    const html = [];
+
+    html.push(team
+        .filter(employee => employee.getRole() === "Manager")
+        .map(manager => generateManager(manager))
+    );
+    html.push(team
+        .filter(employee => employee.getRole() === "Engineer")
+        .map(engineer => generateEngineer(engineer))
+        .join("")
+    );
+    html.push(team
+        .filter(employee => employee.getRole() === "Intern")
+        .map(intern => generateIntern(intern))
+        .join("")
+    );
+
+    return html.join("");
+
+}
+
+module.exports = team => {
+
+    return `
+    <!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title>Dream Team</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <script src="https://kit.fontawesome.com/c502137733.js"></script>
+</head>
+
+<body>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12 jumbotron mb-3 team-heading bg-primary text-white">
+                <h1 class="text-center">The Dream Team</h1>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="team-area col-12 d-flex justify-content-center">
+                ${generateTeam(team)}
+            </div>
+        </div>
+    </div>
+</body>
+</html>
+    `;
+};
